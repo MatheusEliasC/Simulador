6d4c8daf0d4ab1f363cada17ad364f8f8ff01e85
MatheusEliasC <matheus.elias.cruz@hotmail.com>
2021-11-23 20:46:08 -0300

    first commit

---
+++ Components.py
@@ -0,0 +1,302 @@
+import random
+import time
+import threading
+
+TIME_MULTIPLIER = 0.01
+DEBUG = False
+
+class Input:
+    def __init__(self, name, min_qtt, max_qtt, min_periodo, max_periodo, stop_signal):
+        self.stop_signal = stop_signal
+        self.name = name
+        self.input_amount = (min_qtt, max_qtt)
+        self.periodo = (min_periodo, max_periodo)
+        self.output_pipes = []
+        self.inputlock = threading.Lock()
+        self.total_input = 0
+        self.cicles = 0
+
+    def connect_pipe(self, pipe):
+        self.output_pipes.append(pipe)
+
+    def pump(self):
+        while not self.stop_signal():
+            if DEBUG: time.sleep(TIME_MULTIPLIER)
+            amount = random.SystemRandom().uniform(self.input_amount[0], self.input_amount[1])
+            with self.inputlock:
+                self.total_input += amount
+                self.cicles += 1
+            for pipe in self.output_pipes:
+                pipe(amount/len(self.output_pipes), self.name)
+            wait_time = random.SystemRandom().uniform(self.periodo[0], self.periodo[1])
+            time.sleep(wait_time*TIME_MULTIPLIER)
+
+    def start(self):
+        thread = threading.Thread(target=self.pump, name="Input_{}".format(self.name))
+        thread.start()
+        return thread
+
+    def __str__(self):
+        with self.inputlock:
+            log = self.name.ljust(20, ' ')
+            return "{:<20} | {:<25.2f} | {:<6}".format(self.name, self.total_input, self.cicles)
+
+class Pipe:
+    def __init__(self, name, *output_list):
+        self.name = name
+        self.output_list = list(output_list)
+        self.pipe_full = False
+        self.pipelock = threading.Lock()
+
+    def __call__(self, valor, product):
+        if product == None:
+            return valor
+        with self.pipelock:
+            throwback = 0
+            if not self.pipe_full:
+                for output in self.output_list:
+                    throwback += output(valor/len(self.output_list), product)
+        return throwback
+
+class Tank:
+    def __init__(self, capacity, name, stop_signal):
+        self.name = name
+        self.stop_signal = stop_signal
+        self.capacity = capacity
+        self.level = 0
+        self.content = []
+        self.tanklock = threading.Lock()
+        self.output_pipes = []
+        self.cicles = 0
+
+    def connect_pipe(self, pipe):
+        self.output_pipes.append(pipe)
+
+    def pump(self):
+        while not self.stop_signal():
+            if DEBUG: time.sleep(TIME_MULTIPLIER)
+            if len(self.output_pipes) > 0:
+                amount = 0.0
+                product = None
+                with self.tanklock:
+                    try:
+                        amount, product = self.content.pop(0)
+                        self.level -= amount
+                    except:
+                        pass
+                    if amount > 0:
+                        self.cicles += 1
+                throwback = 0
+                for pipe in self.output_pipes:
+                    throwback += pipe(amount/len(self.output_pipes), product)
+                if throwback > 0:
+                    with self.tanklock:
+                        self.content.insert(0, (throwback, product))
+                        self.level += throwback
+
+    def start(self):
+        thread = threading.Thread(target=self.pump, name="Tank_{}".format(self.name))
+        thread.start()
+        return thread
+
+    def __call__(self, qtt, product):
+        with self.tanklock:
+            entry = self.capacity-self.level if qtt + self.level > self.capacity else qtt
+            throwback = qtt - entry
+            self.level += entry
+            if entry > 0:
+                self.content.append((entry, product))
+        return throwback
+
+    def __str__(self):
+        with self.tanklock:
+            products = {}
+            for qtt, product in self.content:
+                try: products[product] += qtt
+                except: products[product] = qtt
+            products = ' / '.join(["{:.2f} ({})".format(amount, product_name).ljust(20, ' ') for product_name, amount in products.items()])
+            status_string = "{:<30} | {:>10} | {:>10.2f} | {:<6} | ".format(self.name, self.capacity, self.level, self.cicles)
+            status_string += products
+        return status_string
+
+
+class Reactor(Tank):
+    def __init__(self, capacity, name, flow, stop_signal):
+        super().__init__(capacity, name, stop_signal)
+        self.flow = flow
+        self.products = {
+                        'NaOH': 0,
+                        'Oleo': 0,
+                        'EtOH': 0
+                        }
+        self.cumulated_output = 0
+
+    def pump(self):
+        while not self.stop_signal():
+            if DEBUG: time.sleep(TIME_MULTIPLIER)
+            with self.tanklock:
+                naoh = min(self.flow/4, self.products['NaOH'])
+                oil  = min(self.flow/4, self.products['Oleo'])
+                etoh = min(self.flow/2, self.products['EtOH'])
+                products_limited = {'NaOH': naoh, 'Oleo': oil, 'EtOH': etoh/2}
+                bottleneck = min(products_limited.keys(), key=(lambda product: products_limited[product]))
+                next_iter = products_limited[bottleneck] == 0
+            if next_iter: continue
+            if bottleneck == 'NaOH':
+                used =  {
+                    'NaOH': naoh,
+                    'Oleo': naoh,
+                    'EtOH': 2*naoh
+                }
+            elif bottleneck == 'Oleo':
+                used =  {
+                    'NaOH': oil,
+                    'Oleo': oil,
+                    'EtOH': 2*oil
+                }
+            elif bottleneck == 'EtOH':
+                used =  {
+                    'NaOH': etoh/2,
+                    'Oleo': etoh/2,
+                    'EtOH': etoh
+                }
+            else:
+                raise Exception("Invalid Product on reactor")
+            total = sum(used.values())
+            with self.tanklock:
+                for product, amount in used.items():
+                    throwback = 0
+                    for pipe in self.output_pipes:
+                        throwback += pipe(amount/len(self.output_pipes), "Mistura")
+                    self.products[product] -= (amount + throwback)
+                    self.cicles += 1
+            self.cumulated_output += total
+            self.level -= total
+            time.sleep(1*TIME_MULTIPLIER)
+            if self.cumulated_output > 3:
+                rest_time = (5*3/self.cumulated_output)-1
+                time.sleep(rest_time*TIME_MULTIPLIER)
+                self.cumulated_output = 0
+
+    def __call__(self, qtt, product):
+        with self.tanklock:
+            entry = self.capacity-self.level if qtt + self.level > self.capacity else qtt
+            throwback = qtt - entry
+            self.level += entry
+            self.products[product] += entry
+            return throwback
+
+    def __str__(self):
+        with self.tanklock:
+            products_string = ' / '.join(["{:.2f} ({})".format(amount, product_name).ljust(20, ' ') for product_name, amount in self.products.items()])
+            status_string = "{:<30} | {:>10} | {:>10.2f} | {:<6} | ".format(self.name, self.capacity, self.level, self.cicles)
+            status_string += products_string
+        return status_string
+
+class Decanter(Tank):
+    def __init__(self, capacity, name, stop_signal):
+        super().__init__(capacity, name, stop_signal)
+        self.glycerin_pipe = None
+        self.etoh_pipe = None
+        self.wash_pipe = None
+
+    def connect_glycerin_pipe(self, pipe):
+        self.glycerin_pipe = pipe
+    def connect_etoh_pipe(self, pipe):
+        self.etoh_pipe = pipe
+    def connect_wash_pipe(self, pipe):
+        self.wash_pipe = pipe
+
+    def pump(self):
+        while not self.stop_signal():
+            if DEBUG: time.sleep(TIME_MULTIPLIER)
+            total = 0
+            with self.tanklock:
+                total = sum([amount for amount, _ in self.content])
+                self.content.clear()
+                self.level -= total
+                if total > 0:
+                    self.cicles += 1
+            throwback = 0
+            throwback += self.glycerin_pipe(0.02*total, 'Glicerina')
+            throwback += self.etoh_pipe(0.09*total, 'EtOH')
+            throwback += self.wash_pipe(0.89*total, 'Lavagem')
+            if throwback > 0:
+                with self.tanklock:
+                    self.content.insert(0, (throwback, 'throwback'))
+                    self.level += throwback
+
+class WashTank(Tank):
+    def __init__(self, capacity, name, loss, stop_signal):
+        super().__init__(capacity, name, stop_signal)
+        self.loss = loss
+
+    def pump(self):
+        while not self.stop_signal():
+            if DEBUG: time.sleep(TIME_MULTIPLIER)
+            if len(self.output_pipes) > 0:
+                amount = 0.0
+                product = None
+                with self.tanklock:
+                    try:
+                        amount, product = self.content.pop(0)
+                        self.level -= amount
+                        amount *= (1-self.loss)
+                    except:
+                        pass
+                    if amount > 0:
+                        self.cicles += 1
+                throwback = 0
+                for pipe in self.output_pipes:
+                    throwback += pipe(amount/len(self.output_pipes), 'Biodisel')
+                if throwback > 0:
+                    with self.tanklock:
+                        self.content.insert(0, (throwback, 'Biodisel'))
+                        self.level += throwback
+
+class Dryer(Tank):
+    def __init__(self, capacity, name, stop_signal, loss, time_per_liter):
+        super().__init__(capacity, name, stop_signal)
+        self.loss = loss
+        self.time_per_liter = time_per_liter
+        self.drying_time = 0
+        self.drying_liters = 0
+
+    def pump(self):
+        while not self.stop_signal():
+            if DEBUG: time.sleep(TIME_MULTIPLIER)
+            if len(self.output_pipes) > 0:
+                amount = 0.0
+                product = None
+                with self.tanklock:
+                    try:
+                        amount, product = self.content[0]
+                        self.level -= amount
+                    except:
+                        pass
+                    self.drying_time = self.time_per_liter*amount
+                    self.drying_liters = amount
+                    if amount > 0:
+                        self.cicles += 1
+                time.sleep(self.time_per_liter*amount*TIME_MULTIPLIER)
+                if amount > 0:
+                    with self.tanklock: self.content.pop(0)
+                throwback = 0
+                for pipe in self.output_pipes:
+                    throwback += pipe(amount*(1-self.loss)/len(self.output_pipes), product)
+                if throwback > 0:
+                    with self.tanklock:
+                        self.content.insert(0, (throwback, product))
+                        self.level += throwback
+
+    def __str__(self):
+        with self.tanklock:
+            products = {}
+            for qtt, product in self.content:
+                try: products[product] += qtt
+                except: products[product] = qtt
+            products = "{:<17}".format(' / '.join(["{:.2f} ({})".format(amount, product_name).ljust(15, ' ') for product_name, amount in products.items()]))
+            drying_string = " | {:.2f} L ({:.2f} seconds)".format(self.drying_liters, self.drying_time)
+            status_string = "{:<30} | {:>10} | {:>10.2f} | {:<6} | ".format(self.name, self.capacity, self.level, self.cicles)
+            status_string += products + drying_string
+        return status_string
---
+++ main.py
@@ -0,0 +1,143 @@
+from Components import *
+import threading
+import signal
+import math
+import time
+import os
+
+SIMULATION_TIME = 36
+
+threads = []
+
+def logging(stop_signal, **components):
+    while not stop_signal():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        print("{:^20} | {:^10} | Cicles".format("Input", "Total injetado no sistema"))
+        print("{}+{}+{}".format("-"*21, '-'*27, "-"*8))
+        for input in components.get('inputs', []):
+            print(input)
+        print("\n\n")
+
+        print("{:^30} | {:^10} | {:^10} | {:^6} | Products".format("Reator", "Capacidade", "Nível", "Cicles"))
+        print("{}+{}+{}+{}+{}".format("-"*31, "-"*12, '-'*12, '-'*8, '-'*60))
+        for r in components.get('reactors', []):
+            print(r)
+        print("\n\n")
+
+        print("{:^30} | {:^10} | {:^10} | {:^6} | Products".format("Decantador", "Capacidade", "Nível", "Cicles"))
+        print("{}+{}+{}+{}+{}".format("-"*31, "-"*12, '-'*12, '-'*8, '-'*15))
+        for decanter in components.get('decanters', []):
+            print(decanter)
+        print("\n\n")
+
+        print("{:^30} | {:^10} | {:^10} | {:^6} | {:^17} | Drying".format("Secador", "Capacidade", "Nível", "Cicles", "Products"))
+        print("{}+{}+{}+{}+{}+{}".format("-"*31, "-"*12, '-'*12, '-'*8, '-'*19, '-'*20))
+        for dryer in components.get('dryers', []):
+            print(dryer)
+        print("\n\n")
+
+        print("{:^30} | {:^10} | {:^10} | {:^6} | Products".format("Tanque", "Capacidade", "Nível", "Cicles"))
+        print("{}+{}+{}+{}+{}".format("-"*31, "-"*12, '-'*12, '-'*8, '-'*15))
+        for tank in components.get('tanks', []):
+            print(tank)
+
+        time.sleep(0.5)
+        
+        
+    print("Finished executing...")
+    print("Stoping threads...")
+
+stop_lock = threading.Lock()
+stop_variable = False
+def stop_signal():
+    global stop_variable, stop_lock
+    with stop_lock: 
+        return stop_variable
+def stop_handler(num, stack):
+    global stop_variable
+    stop_variable = True
+signal.signal(signal.SIGINT, stop_handler)
+
+input_oil = Input("Oleo", 1, 2, 0, 10, stop_signal)
+input_NaOH = Input("NaOH", 0.25, 0.25, 1, 1, stop_signal)
+input_EtOH = Input("EtOH", 0.125, 0.125, 1, 1, stop_signal)
+tank_oil = Tank(math.inf, "Tanque de oleo", stop_signal)
+tank_naoh_etoh = Tank(math.inf, "Tanque de NaOH/EtOH", stop_signal)
+tank_glycerin = Tank(math.inf, "Tanque de Glicerina", stop_signal)
+tank_biodisel = Tank(math.inf, "Tanque de Biodisel", stop_signal)
+tank_etoh = Tank(math.inf, "Tanque de EtOH (Reaproveitado)", stop_signal)
+reactor = Reactor(math.inf, "Reator",5, stop_signal)
+decanter = Decanter(10, "Decantador", stop_signal)
+
+washing_tank_1 = WashTank(math.inf, "Tanque de Lavagem 1", 0.075, stop_signal)
+washing_tank_2 = WashTank(math.inf, "Tanque de Lavagem 2", 0.075, stop_signal)
+washing_tank_3 = WashTank(math.inf, "Tanque de Lavagem 3", 0.075, stop_signal)
+
+dryer_etoh = Dryer(math.inf, "Secador de EtOH", stop_signal, 0.03, 5)
+dryer_biodisel = Dryer(math.inf, "Secador de Lavagem", stop_signal, 0.03, 5)
+
+pipe_oil_tank = Pipe("pipe(Oleo)", tank_oil)
+pipe_naoh_tank = Pipe("pipe(NaOH)", tank_naoh_etoh)
+pipe_etoh_tank = Pipe("pipe(EtOH)", tank_naoh_etoh)
+pipe_oil_reactor = Pipe("pipe(Oleo)", reactor)
+pipe_naoh_etoh_reactor = Pipe("Pipe(NaOH/EtOH)", reactor)
+pipe_reactor_decanter = Pipe("Pipe(NaOh/2EtOH/Oleo)", decanter)
+pipe_decanter_dryer = Pipe("Pipe(EtOH) decanter", dryer_etoh)
+pipe_decanter_glycerin = Pipe("Pipe(Glicerina)", tank_glycerin)
+pipe_decanter_washing_tank_1 = Pipe("Pipe(Lavagem)", washing_tank_1)
+pipe_washing_tank_1_washing_tank_2 = Pipe("Pipe(Lavagem)", washing_tank_2)
+pipe_washing_tank_2_washing_tank_3 = Pipe("Pipe(Lavagem)", washing_tank_3)
+pipe_washing_tank_3_dryer_biodisel = Pipe("Pipe(Lavagem)", dryer_biodisel)
+pipe_dryer_etoh_tank = Pipe("Pipe(EtOH)", tank_etoh)
+pipe_etoh_tank_naoh_etoh = Pipe("Pipe(EtOH[R])",tank_naoh_etoh)
+pipe_dryer_biodisel_tank = Pipe("Pipe(Biodisel)", tank_biodisel)
+
+input_oil.connect_pipe(pipe_oil_tank)
+input_NaOH.connect_pipe(pipe_naoh_tank)
+input_EtOH.connect_pipe(pipe_etoh_tank)
+tank_oil.connect_pipe(pipe_oil_reactor)
+tank_naoh_etoh.connect_pipe(pipe_naoh_etoh_reactor)
+reactor.connect_pipe(pipe_reactor_decanter)
+decanter.connect_etoh_pipe(pipe_decanter_dryer)
+decanter.connect_glycerin_pipe(pipe_decanter_glycerin)
+decanter.connect_wash_pipe(pipe_decanter_washing_tank_1)
+washing_tank_1.connect_pipe(pipe_washing_tank_1_washing_tank_2)
+washing_tank_2.connect_pipe(pipe_washing_tank_2_washing_tank_3)
+washing_tank_3.connect_pipe(pipe_washing_tank_3_dryer_biodisel)
+tank_etoh.connect_pipe(pipe_etoh_tank_naoh_etoh)
+dryer_etoh.connect_pipe(pipe_dryer_etoh_tank)
+dryer_biodisel.connect_pipe(pipe_dryer_biodisel_tank)
+
+log_thread = threading.Thread(
+    target=logging, 
+    name="logging_tool", 
+    args=([stop_signal]),
+    kwargs={
+            'inputs': [input_oil, input_NaOH, input_EtOH],
+            'dryers': [dryer_etoh, dryer_biodisel],
+            'reactors': [reactor],
+            'decanters': [decanter],
+            'tanks': [tank_oil, tank_naoh_etoh, tank_glycerin, tank_etoh, 
+                      washing_tank_1, washing_tank_2, washing_tank_3, tank_biodisel]
+            })
+
+threads.append(input_oil.start())
+threads.append(input_NaOH.start())
+threads.append(input_EtOH.start())
+threads.append(tank_oil.start())
+threads.append(tank_naoh_etoh.start())
+threads.append(reactor.start())
+threads.append(decanter.start())
+threads.append(washing_tank_1.start())
+threads.append(washing_tank_2.start())
+threads.append(washing_tank_3.start())
+threads.append(dryer_biodisel.start())
+threads.append(dryer_etoh.start())
+threads.append(tank_etoh.start())
+threads.append(log_thread.start())
+
+time.sleep(SIMULATION_TIME)
+stop_variable = True
+for thread in threads:
+    if thread: thread.join()
